560924de0ee08ba2017127605a6e76c2b1972c7e
Bálint Pölczman <polczman.balint@grape.hu>
2023-02-17 21:14:11 +0100

    add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
 import "antd/dist/reset.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./app/query";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
